b93af25d03bf94d7a293c059bb5e5fad0e2c3dec
David Woolf <davidjohnwoolf@gmail.com>
2018-01-29 00:13:55 +0000

    setup express server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.listen(8080, function() {
+    console.log(`listening on port ${port}...`);
+});
