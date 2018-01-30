e0460c0cf2f4890e5a10c9c1ba27fc57e3b562c4
David Woolf <davidjohnwoolf@gmail.com>
2018-01-30 03:57:02 +0000

    set root route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.get('/', (req, res) => {
+    res.send('working');
+});
+
 app.listen(8080, function() {
     console.log(`listening on port ${port}...`);
 });
