133fee135d64fefecf15faf9294db6ba4fe588bd
David Woolf <davidjohnwoolf@gmail.com>
2018-01-30 22:25:29 +0000

    install bcrypt and add to user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
-const UserSchema = new Schema({
+const userSchema = new Schema({
     email: { type: String, required: true, index: { unique: true }, match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ },
     username: { type: String, required: true },
     password: { type: String, required: true },
@@ -19,7 +20,7 @@ userSchema.pre('save', function(next) {
         if (err) return next(err);
         
         // hash the password combined with the salt
-        bcrypt.hash(user.password, salt, (err, hash) {
+        bcrypt.hash(user.password, salt, (err, hash) => {
             if (err) return next(err);
             
             user.password = hash;
