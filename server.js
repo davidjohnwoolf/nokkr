de0b21336d0b5ecbb0d9e8330e4af980fc4c71a1
David Woolf <davidjohnwoolf@gmail.com>
2018-01-30 21:40:53 +0000

    set up database

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+// load environment variables from .env file
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
 const port = process.env.PORT || 8080;
 
 const Users = require('./controllers/users');
 
+// connect database from MongoDB Atlas
+mongoose.connect(`mongodb+srv://platform2kadmin:${process.env.DB_PASSWORD}@platform2k-2kpxf.mongodb.net/test`);
+
+//get default connection
+const db = mongoose.connection;
+
+// bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// static files
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/user', Users);
