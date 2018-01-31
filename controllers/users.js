55561c62aff1e20caabca5f59fb3b5e3f1fdceb4
David Woolf <davidjohnwoolf@gmail.com>
2018-01-31 06:08:32 +0000

    add user index and user create

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const User = require('../models/user');
 
 // body parser middleware
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
+// index
 router.get('/', (req, res) => {
-    res.send('user');
+    User.find({}, (err, users) => {
+        if (err) return res.json(err);
+        
+        return res.json(users);
+    });
+});
+
+// create
+router.post('/', (req, res) => {
+    User.findOne({ username: req.body.username }, (err, user) => {
+        if (user) return res.json({ errorMessage: 'Username exists' });
+        
+        if (req.body.password === req.body.passwordConfirmation) {
+            const user = {};
+            
+            for (let value in req.body) {
+                user[value] = req.body[value];
+            }
+            
+            user.save(() => {
+                if (err) return res.json(error);
+                
+                return res.json({ successMessage: 'User created successfully' });
+            });
+            
+        } else {
+            return res.json({ errorMessage: 'Passwords do not match' });
+        }
+    });
 });
 
 module.exports = router;
