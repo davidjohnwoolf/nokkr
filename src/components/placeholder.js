06593f6b690431655a9c0ff91cc74cb972a9828d
David Woolf <davidjohnwoolf@gmail.com>
2018-02-10 03:06:27 +0000

    rename placeholder

---
+++ src/components/placeholder.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Placeholder extends React.Component {
+    
+    render() {
+        return (
+            <h1>Platform2K</h1>
+        );
+    }
+}
+
+export default Placeholder;
