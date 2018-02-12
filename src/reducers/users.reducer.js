e7bee1389ec3142c58ce6c9556b802f5a829fada
David Woolf <davidjohnwoolf@gmail.com>
2018-02-12 15:00:07 +0000

    update user show componments

--- src/reducers/users.reducer.js
+++ src/reducers/users.reducer.js
@@ -3,7 +3,7 @@ import { FETCH_USER } from '../actions/users.action';
 export default function(state = {}, action) {
 
     switch (action.type) {
-        case 'FETCH_USER':
+        case FETCH_USER:
             return { ...state, user: action.payload.data };
         
         default:
