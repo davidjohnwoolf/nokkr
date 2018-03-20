a20fb8331503c2ffa076793289dee98c05485f13
David Woolf <davidjohnwoolf@gmail.com>
2018-03-20 21:02:40 +0000

    add edit and delete buttons

--- src/components/users/user-show.js
+++ src/components/users/user-show.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import { fetchUser } from '../../actions/users.action';
+import { fetchUser, deleteUser } from '../../actions/users.action';
 
 class UserShow extends React.Component {
     
-	constructor(props) {
-        super(props);
+	componentDidMount() {
         this.props.fetchUser(this.props.match.params.id);
+    }
+    
+    handleDelete() {
+        if (confirm('Are you sure you want to delete this user?')) {
+            this.props.deleteUser(this.props.match.params.id);
+            this.props.history.push(`/user/?message=Deleted+User+Successfully`);
+        }
     }
     
     renderUser() {
@@ -16,10 +23,16 @@ class UserShow extends React.Component {
         if (!user) return;
         
         return (
-            <div>
-                <h1>{ user.name } <span className="edit-icon fa fa-pencil-square-o"></span></h1>
-                <h4>{ user.username } <span className="edit-icon fa fa-pencil-square-o"></span></h4>
-                <address>{ user.email } <span className="edit-icon fa fa-pencil-square-o"></span></address>
+            <div className="component-page user-show">
+                <h1>{ user.name }</h1>
+                <h4>{ user.username }</h4>
+                <address>{ user.email }</address>
+                <Link to={ `/user/${user.id}/edit` } className="edit-button">
+		            Edit User
+		        </Link>
+                <button onClick={ this.handleDelete.bind(this) } className="delete-button">
+		            Delete User
+		        </button>
             </div>
         );
     }
@@ -39,4 +52,4 @@ const mapStateToProps = state => {
 };
 
 
-export default connect(mapStateToProps, { fetchUser })(UserShow);
+export default connect(mapStateToProps, { fetchUser, deleteUser })(UserShow);
