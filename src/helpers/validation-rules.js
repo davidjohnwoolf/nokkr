dfba6ae649805820aade6ac824a0126a80ec8552
David Woolf <davidjohnwoolf@gmail.com>
2018-05-13 22:21:11 +0000

    formatting

--- src/helpers/validation-rules.js
+++ src/helpers/validation-rules.js
@@ -1,4 +1,5 @@
 export const required = value => value ? undefined : 'Required';
+
 export const password = value => {
     return (
         value && /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,24}/.test(value)
@@ -6,10 +7,10 @@ export const password = value => {
             : 'Password must contain 8-24 characters including a number, an uppercase and lowercase letter, and a special character'
     );
 };
+
 export const passwordMatch = () => {
     return (
-        document.querySelector('input[name=password]').value === document.querySelector('input[name=passwordConfirmation]').value
-            ? undefined
-            : 'Passwords must match'
+        document.querySelector('[name=password]').value === document.querySelector('[name=passwordConfirmation]').value
+            ? undefined : 'Passwords must match'
     );
 };
