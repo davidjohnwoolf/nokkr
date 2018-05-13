a64b377c289560c7e35f0ae3169240d37b68ca0d
David Woolf <davidjohnwoolf@gmail.com>
2018-05-13 03:15:57 +0000

    export as variables

--- src/helpers/validation-rules.js
+++ src/helpers/validation-rules.js
@@ -1,19 +1,15 @@
-const validationRules = {
-    required: value => value ? undefined : 'Required',
-    password: value => {
-        return (
-            value && /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,24}/.test(value)
-                ? undefined
-                : 'Password must contain 8-24 characters including a number, an uppercase and lowercase letter, and a special character'
-        );
-    },
-    passwordMatch: () => {
-        return (
-            document.querySelector('input[name=password]').value === document.querySelector('input[name=passwordConfirmation]').value
-                ? undefined
-                : 'Passwords must match'
-        );
-    }
+export const required = value => value ? undefined : 'Required';
+export const password = value => {
+    return (
+        value && /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,24}/.test(value)
+            ? undefined
+            : 'Password must contain 8-24 characters including a number, an uppercase and lowercase letter, and a special character'
+    );
 };
-
-export default validationRules;
+export const passwordMatch = () => {
+    return (
+        document.querySelector('input[name=password]').value === document.querySelector('input[name=passwordConfirmation]').value
+            ? undefined
+            : 'Passwords must match'
+    );
+};
