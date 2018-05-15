78d4808982a20dbd1023ace88a77bc9162dab34a
David Woolf <davidjohnwoolf@gmail.com>
2018-05-15 20:04:25 +0000

    update password

--- src/helpers/validation-rules.js
+++ src/helpers/validation-rules.js
@@ -2,7 +2,7 @@ export const required = value => value ? undefined : 'Required';
 
 export const password = value => {
     return (
-        value && /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,24}/.test(value)
+        (!value || /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,24}/.test(value))
             ? undefined
             : 'Password must contain 8-24 characters including a number, an uppercase and lowercase letter, and a special character'
     );
