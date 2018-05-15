809bb25976fb7db6c4fbf18683aafad59675e961
David Woolf <davidjohnwoolf@gmail.com>
2018-05-15 20:04:07 +0000

    add update action

--- src/actions/users.js
+++ src/actions/users.js
@@ -4,7 +4,11 @@ export const FETCH_USERS = 'FETCH_USERS';
 export const FETCH_USER = 'FETCH_USER';
 export const CREATE_USER_SUCCESS = 'CREATE_USER_SUCCESS';
 export const CREATE_USER_ERROR = 'CREATE_USER_ERROR';
+export const UPDATE_USER_SUCCESS = 'UPDATE_USER_SUCCESS';
+export const UPDATE_USER_ERROR = 'UPDATE_USER_ERROR';
 export const CLEAR_USER_MESSAGES = 'CLEAR_USER_MESSAGES';
+
+//why are you getting the console error when not using those methods?
 
 export const fetchUsers = () => {
     const request = axios.get('/users');
@@ -45,6 +49,31 @@ export const createUser = user => {
             if (res.data.message) {
                 dispatch({
                     type: CREATE_USER_SUCCESS,
+                    successMessage: res.data.message
+                });
+            }
+        });
+    }
+};
+
+export const updateUser = (id, user) => {
+    //maybe use async await and fetch
+    //is it bad you are using the same variables for user edit and user create?
+    const request = axios.put(`/users/${id}`, user);
+    
+    return dispatch => {
+        request.then(res => {
+
+            if (res.data.error) {
+                dispatch({
+                    type: UPDATE_USER_ERROR,
+                    serverError: res.data.error
+                });
+            }
+            
+            if (res.data.message) {
+                dispatch({
+                    type: UPDATE_USER_SUCCESS,
                     successMessage: res.data.message
                 });
             }
