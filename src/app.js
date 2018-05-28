a15c3f5899ab4440a409b19f78f9c4bd7295b111
David Woolf <davidjohnwoolf@gmail.com>
2018-05-28 21:56:18 +0000

    set up dashboard base

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import PrivateRoute from './components/helpers/private-route';
 import FlashMessage from './components/helpers/flash-messages';
 import NotAuthorized from './components/errors/not-authorized';
 import PageNotFound from './components/errors/page-not-found';
+import Dashboard from './components/dashboard';
 import UserNew from './components/users/user-new';
 import UserEdit from './components/users/user-edit';
 import UserShow from './components/users/user-show';
@@ -40,6 +41,7 @@ ReactDOM.render(
                         <PrivateRoute path="/users/:id/edit" component={ UserEdit } />
                         <PrivateRoute path="/users/:id" component={ UserShow } />
                 		<PrivateRoute path="/users" component={ UserIndex } />
+                		<PrivateRoute path="/" component={ Dashboard } />
                 		<Route path="*" component={ PageNotFound } />
             		</Switch>
                 </div>
---
+++ src/components/dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Dashboard = () => (
+    <main id="page-not-found" className="content">
+        <section className="dashboard">
+            <h1>Platform 2K</h1>
+        </section>
+    </main>
+);
+
+export default Dashboard;
