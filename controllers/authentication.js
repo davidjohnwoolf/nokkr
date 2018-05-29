8b0ad604462f383641713ec643c807deebc2047e
David Woolf <davidjohnwoolf@gmail.com>
2018-05-29 02:18:43 +0000

    add comment

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -39,4 +39,35 @@ router.post('/login', (req, res) => {
     });
 });
 
+// revoke jwt
+/*router.delete('/logout', (req, res) => {
+    User.findOne({ $or: [{username: req.body.username}, { email: req.body.email }] }, (err, user) => {
+        if (err) return res.json(err);
+        
+        if (!user) {
+            return res.json({ error: 'Username or password is incorrect' });
+        } else {
+            
+            user.comparePassword(req.body.password, function(err, isMatch) {
+                if (err) return res.json(err);
+                
+                if (isMatch) {
+                    
+                    let token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+                        // expires in 24 hours
+                        expiresIn: 86400
+                    });
+                    
+                    res.json({ message: 'Logged in', token: token });
+                }
+                
+                if (!isMatch) {
+                    res.json({ error: 'Username or password is incorrect' });
+                }
+            });
+            
+        }
+    });
+});*/
+
 module.exports = router;
