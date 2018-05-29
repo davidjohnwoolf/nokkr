16b09bc5c38712f1ec91dc7bf9e8c66631bb0be6
David Woolf <davidjohnwoolf@gmail.com>
2018-05-29 02:42:56 +0000

    remove console logs

--- src/helpers/authorization.js
+++ src/helpers/authorization.js
@@ -12,28 +12,20 @@ export default function authorization(token, store) {
 	    //decode token
 		const decoded = jwtDecode(token);
 		const currentTime = Date.now() / 1000;
-		console.log('authorization');
-		
-		console.log(decoded);
-		console.log(currentTime);
 		
 		//check exp
 		if (currentTime < decoded.exp) {
 		    //set auth header
 		    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 		    store.dispatch({ type: AUTHENTICATED, id: decoded.id });
-		    console.log('authorized');
 		} else {
 		    sessionStorage.removeItem('p2k_token');
 		    delete axios.defaults.headers.common['Authorization'];
 	        store.dispatch({ type: UNAUTHENTICATED });
-	        console.log('not authorized');
 		}
         
 	} else {
 	    delete axios.defaults.headers.common['Authorization'];
 	    store.dispatch({ type: UNAUTHENTICATED });
-	    
-	    console.log('no token');
 	}
 }
