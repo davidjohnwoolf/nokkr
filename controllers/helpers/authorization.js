68224c3a4e647467b5fe65e42ce0088d81633a99
David Woolf <davidjohnwoolf@gmail.com>
2018-05-30 17:07:44 +0000

    update to send correct error status

--- controllers/helpers/authorization.js
+++ controllers/helpers/authorization.js
@@ -2,19 +2,19 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
 
-    if (req.headers) {
+    if (req.get('Authorization')) {
+        
         const token = req.get('Authorization').split(' ')[1];
         
         jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
-            //res.status(403).send({ auth: false, message: 'Failed to authenticate token.' });
-            if (err) return res.json({ error: err, errorMessage: 'Authentication Failed' });
+            if (err) return res.status(401).json({ error: true, message: 'Authenticatiom failed' });
 
             next();
         });
+        
     } else {
-        //set up 401
-        //res.status(401).send({ auth: false, message: 'Failed to authenticate token.' });
-        res.json({ error: true, errorMessage: 'Authentication Failed' });
+        
+        res.status(401).json({ error: true, message: 'Authenticatiom failed' });
     }
 };
 
