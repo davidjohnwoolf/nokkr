6bf79558cc0a42f7bc08112c4320ad468a6de56f
David Woolf <davidjohnwoolf@gmail.com>
2018-06-29 16:51:10 +0000

    update role

--- src/components/helpers/private-route.js
+++ src/components/helpers/private-route.js
@@ -9,16 +9,16 @@ import { SU, ADMIN, MANAGER, USER } from '../../../controllers/helpers/api-varia
 class PrivateRoute extends React.Component {
     
     render() {
-        const { component: Component, authenticated, access, isSuperUser, isAdmin, isManager, ...rest } = this.props;
+        const { component: Component, authenticated, access, role, ...rest } = this.props;
         
         const permitted = (function(a) {
-            if (isSuperUser) return true;
+            if (role === SU) return true;
             
-            if (isAdmin && (a !== SU)) return true;
+            if (role === ADMIN && (a !== SU)) return true;
             
-            if (isManager && (a !== SU) && (a !== ADMIN)) return true;
+            if (role === MANAGER && (a !== SU) && (a !== ADMIN)) return true;
             
-            if (!isManager && (a !== SU) && (a !== ADMIN) && (a !== MANAGER)) return true;
+            if (role === USER && (a !== SU) && (a !== ADMIN) && (a !== MANAGER)) return true;
             
             return false;
         })(access);
