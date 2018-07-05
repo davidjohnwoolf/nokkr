49e96136252b2003deb3f715e615c5a2998ece8c
David Woolf <davidjohnwoolf@gmail.com>
2018-07-05 18:40:46 +0000

    add condition of authorized

--- src/components/errors/not-authorized.js
+++ src/components/errors/not-authorized.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const NotAuthorized = () => (
-    <main id="not-authorized" className="content">
-        <section className="error-page">
-            <h1>403 Not Authorized</h1>
-        </section>
-    </main>
-);
+class NotAuthorized extends React.Component {
+    
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        const { authenticated } = this.props;
+        
+        return (
+            <main id="not-authorized" className="content">
+                <section className="error-page">
+                    <h1>403 Not Authorized</h1>
+                    { !authenticated ? <Link className="btn btn-primary" to="/login">Login</Link> : <h4>You do not have permission to see this</h4> }
+                </section>
+            </main>
+        )
+    };
+}
 
-export default NotAuthorized;
+const mapStateToProps = state => ({
+    authenticated: state.auth.authenticated
+});
+
+
+export default connect(mapStateToProps)(NotAuthorized);
