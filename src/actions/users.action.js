c3191f61b3faa1455ba408a581ad3055e511c783
David Woolf <davidjohnwoolf@gmail.com>
2018-07-09 22:49:10 +0000

    format

--- src/actions/users.action.js
+++ src/actions/users.action.js
@@ -30,7 +30,6 @@ export const fetchUser = id => {
         
         if (response.data.status === ERROR) dispatch(sendError(response.data.message));
         
-        
         dispatch({ type: FETCH_USER, user: response.data.data.user });
     };
 };
