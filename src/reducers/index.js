9c489dcb81d313611c6ece0580e67a7d7363c3e5
David Woolf <davidjohnwoolf@gmail.com>
2018-07-12 17:35:33 +0000

    add area group reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import users from './users.reducer';
 import auth from './auth.reducer';
 import flash from './flash.reducer';
 import areas from './areas.reducer';
+import areaGroups from './area-groups.reducer';
 import teams from './teams.reducer';
 
 export default combineReducers({
-	users,
-	auth,
 	flash,
+	auth,
+	users,
+	teams,
 	areas,
-	teams
+	areaGroups
 });
