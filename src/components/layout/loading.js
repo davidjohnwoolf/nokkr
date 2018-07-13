cabc8a1b6aa9fb1c084e1586b424c1bf43c463af
David Woolf <davidjohnwoolf@gmail.com>
2018-07-13 15:46:29 +0000

    create loading component

---
+++ src/components/layout/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <section className="spinner">
+            <i className="fas fa-spinner fa-spin"></i>
+        </section>
+    );
+};
+
+export default Loading;
