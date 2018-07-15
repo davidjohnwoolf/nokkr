d0a820d38001e5bee7f4189b528057721be9fb06
David Woolf <davidjohnwoolf@gmail.com>
2018-07-15 19:30:15 +0000

    refactor

--- src/components/errors/not-authorized.js
+++ src/components/errors/not-authorized.js
@@ -4,10 +4,6 @@ import { Link } from 'react-router-dom';
 
 class NotAuthorized extends React.Component {
     
-    constructor(props) {
-        super(props);
-    }
-    
     render() {
         const { authenticated } = this.props;
         
